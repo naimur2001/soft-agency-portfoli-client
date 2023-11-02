3b86d4dc37516f03de94f57c02b2cce3823f688a
naimur2001 <naimurrahman18thapril@gmail.com>
2023-11-02 15:30:08 +0600

    adminlogin updated

--- src/Dashboard/AdminLogin.jsx
+++ src/Dashboard/AdminLogin.jsx
@@ -4,11 +4,17 @@ import { AuthContext } from './AuthProvider';
 import { useContext } from 'react';
 
 const AdminLogin = () => {
+  // admin login
   const {signIn}=useContext(AuthContext)
+
+  //error usestate
   const [error,SetError]=useState('')
+// naviation element
   const location=useLocation();
   const navigate=useNavigate();
   const from=location?.state?.from?.pathname || '/'
+
+  // login function
   const handleLogin=(event)=>{
     event.preventDefault();
     const form = event.target;
@@ -44,6 +50,7 @@ const AdminLogin = () => {
     <div className='my-5 font-fancy'>
 <div className='text-5xl'>Hello Admin</div>
 <div className='lg:w-[50%] w-[100%] mt-20'>
+  {/* login form */}
   <form  onSubmit={handleLogin} className='grid grid-cols-1 gap-5' >
 <input  className='px-3 py-2 bg-transparent border border-slate-400 rounded-xl' type="text" name='email' placeholder='Put 
 Admin Email' />
